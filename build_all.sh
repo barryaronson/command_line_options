4fa2664e8ae6d77a74f017778327b631965cada1
Barry Aronson <baronson@outlook.com>
2024-10-21 14:09:40 -0700

    Added include, example and test directories.

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,9 @@
 echo Building first_n_w_help
-g++ --std=c++17 -o first_n_w_help -g -O0 -Wall first_n_w_help.cpp
+g++ --std=c++17 -o first_n_w_help -g -O0 -Wall -I include example/first_n_w_help.cpp
 echo Building first_n
-g++ --std=c++17 -o first_n -g -O0 -Wall first_n.cpp
+g++ --std=c++17 -o first_n -g -O0 -Wall -I include example/first_n.cpp
 echo Building cloh
-g++ --std=c++17 -o cloh -g -O0 -Wall test_command_line_options_w_help.cpp
+g++ --std=c++17 -o cloh -g -O0 -Wall -I include test/test_command_line_options_w_help.cpp
 echo Building clo
-g++ --std=c++17 -o clo -g -O0 -Wall test_command_line_options.cpp
+g++ --std=c++17 -o clo -g -O0 -Wall -I include test/test_command_line_options.cpp
 echo Done
